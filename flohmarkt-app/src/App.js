abffc8fadd9dac1b27e8e2558ebedbd75d4d8a39
Ecem Onkol <114227090+ecemonkol@users.noreply.github.com>
2023-06-27 14:10:45 +0300

    links are working

--- flohmarkt-app/src/App.js
+++ flohmarkt-app/src/App.js
@@ -1,24 +1,19 @@
 import * as React from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import Contact from './pages/Contact';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/cart">Cart</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar/>
       <Routes>
-        <Route path="/home" element={<Home />}></Route>
+        
+        <Route path="/" element={<Home />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </>
   );
---
+++ flohmarkt-app/src/pages/Contact.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Contact() {
+  return <span>Contact helooo</span>;
+}
+
+export default Contact;
