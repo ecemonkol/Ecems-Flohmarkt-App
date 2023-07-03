2752b70d83b2741173d9194cec4c9fe657356efe
Ecem Onkol <114227090+ecemonkol@users.noreply.github.com>
2023-07-03 17:19:41 +0300

    navbar will be fixed

--- flohmarkt-app/src/components/Navbar.js
+++ flohmarkt-app/src/components/Navbar.js
@@ -3,22 +3,20 @@ import { Link } from 'react-router-dom';
 import { PiShoppingCartLight } from "react-icons/pi";
 import { PiHouseLight } from "react-icons/pi";
 import { PiHeadphonesLight } from "react-icons/pi";
+import "./Navbar.css";
+
 
 
 export const Navbar = () => {
-  return (
-    <nav>
-        <ul>
-          <li>
-            <Link to="/">Shop <PiHouseLight /></Link>
-          </li>
-          <li>
-            <Link to="/cart">Cart <PiShoppingCartLight /></Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact <PiHeadphonesLight /></Link>
-          </li>
-        </ul>
-      </nav>
-  )
-}
+    return (
+      <div className="navbar">
+        <div className="links">
+          <Link to="/"> Shop <PiHouseLight /></Link>
+          <Link to="/contact"> Contact <PiHeadphonesLight /></Link>
+          <Link to="/cart" >
+            Cart<PiShoppingCartLight />
+          </Link>
+        </div>
+      </div>
+    );
+  };
