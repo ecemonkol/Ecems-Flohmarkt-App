40780130f00347ed1a823b3dd9fe992a0b8bbcf6
Ecem Onkol <114227090+ecemonkol@users.noreply.github.com>
2023-07-19 00:04:19 +0200

    css

--- flohmarkt-app/src/pages/Contact/Contact.js
+++ flohmarkt-app/src/pages/Contact/Contact.js
@@ -1,12 +1,23 @@
 import * as React from 'react';
+import "./Contact.css";
 
-export const Contact = () =>  {
+export const Contact = () => {
   return (
-    <div className = "contact">
-      <div>
-         <h3>E-mail me</h3>
+    <form id="contact-form"  method="POST">
+      <div className="form-group">
+        <label className= "name "htmlFor="name">Your Name</label>
+        <input placeholder="Name"type="text" className="form-control1" />
       </div>
-    </div>
+      <div className="form-group">
+        <label htmlFor="exampleInputEmail1">Your e-mail address</label>
+        <input placeholder="example@example.com" type="email" className="form-control" aria-describedby="emailHelp" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="message">Your Message</label>
+        <textarea placeholder="I wanted to reach out to ask.." className="form-control" rows="5"></textarea>
+      </div>
+      <button type="submit" className="button">Submit</button>
+    </form>
   )
 }
 
