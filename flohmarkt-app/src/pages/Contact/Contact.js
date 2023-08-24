a1b9e388ee01998dd37acffbce1308934bbb747c
Ecem Onkol <114227090+ecemonkol@users.noreply.github.com>
2023-08-24 02:38:49 +0200

    responsive css

--- flohmarkt-app/src/pages/Contact/Contact.js
+++ flohmarkt-app/src/pages/Contact/Contact.js
@@ -3,21 +3,24 @@ import "./Contact.css";
 
 export const Contact = () => {
   return (
-    <form className="contactform"  method="POST">
-      <div className="form-group">
-        <label className= "name "htmlFor="name">Your Name</label>
-        <input placeholder="Name"type="text" className="form-control1" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="exampleInputEmail1">Your e-mail address</label>
-        <input placeholder="example@example.com" type="email" className="form-control" aria-describedby="emailHelp" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="message">Your Message</label>
-        <textarea placeholder="I wanted to reach out to ask.." className="form-control" rows="5"></textarea>
-      </div>
-      <button type="submit" className="submitbutton">Submit</button>
-    </form>
+    <div className='container'>
+      <form className="contactform" method="POST">
+        <div className="form-group">
+          <label className="name " htmlFor="name">Your Name</label>
+          <input placeholder="Name" type="text" className="form-control1" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Your e-mail address</label>
+          <input placeholder="example@example.com" type="email" className="form-control" aria-describedby="emailHelp" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Your Message</label>
+          <textarea placeholder="I wanted to reach out to ask.." className="form-control" rows="5"></textarea>
+        </div>
+        <button type="submit" className="submitbutton">Submit</button>
+      </form>
+    </div>
+    
   )
 }
 
